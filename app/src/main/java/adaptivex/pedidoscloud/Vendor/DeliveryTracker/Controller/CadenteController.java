c40de7faff7d1eaa487aefd6c9a62b8dfa3add86
ezgatrabajo <ezgatrabajo@gmail.com>
2018-05-18 11:42:32 -0300

    p

---
+++ app/src/main/java/adaptivex/pedidoscloud/Vendor/DeliveryTracker/Controller/CadenteController.java
@@ -0,0 +1,8 @@
+package adaptivex.pedidoscloud.Vendor.DeliveryTracker.Controller;
+
+/**
+ * Created by egalvan on 18/5/2018.
+ */
+
+public class CadenteController {
+}
--- app/src/main/java/adaptivex/pedidoscloud/Vendor/DeliveryTracker/Entity/Cadete.java
+++ app/src/main/java/adaptivex/pedidoscloud/Vendor/DeliveryTracker/Entity/Cadete.java
@@ -1,10 +1,14 @@
 package adaptivex.pedidoscloud.Vendor.DeliveryTracker.Entity;
 
+import adaptivex.pedidoscloud.Model.User;
+
 /**
  * Created by egalvan on 17/5/2018.
  */
 
 public class Cadete {
+    private User user;
+    private int ubicacionActual;
 
 
 
