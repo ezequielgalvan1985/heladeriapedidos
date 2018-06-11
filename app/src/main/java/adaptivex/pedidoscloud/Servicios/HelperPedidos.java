8746a13e8071604a9debf4c3417e3c0481e9de9d
ezgatrabajo <ezgatrabajo@gmail.com>
2018-06-11 17:25:02 -0300

    p

--- app/src/main/java/adaptivex/pedidoscloud/Servicios/HelperPedidos.java
+++ app/src/main/java/adaptivex/pedidoscloud/Servicios/HelperPedidos.java
@@ -234,7 +234,7 @@ public class HelperPedidos extends AsyncTask<Void, Void, Void> {
             JSONArray pedidodetalles = new JSONArray();
 
             pedido.put("id", String.valueOf(paramPedido.getId()));
-            pedido.put("fecha", String.valueOf(fechahoystr));
+            //pedido.put("fecha", String.valueOf(fechahoystr));
             pedido.put("empresa_id", String.valueOf(GlobalValues.getINSTANCIA().getUserlogued().getEntidad_id()));
             pedido.put("user_id", String.valueOf(GlobalValues.getINSTANCIA().getUserlogued().getId()));
             pedido.put("android_id", String.valueOf(paramPedido.getIdTmp()));
